3aaa7147398c5ce556d22a4da20039c184f949ae
srazhov <srazhov@icloud.com>
2024-06-29 12:47:27 +0200

    Added Testing project

---
+++ PhysHelper/PhysHelper.Tests/SIObjects/Forces/ForceTests.cs
@@ -0,0 +1,17 @@
+﻿namespace PhysHelper.Tests.SIObjects.Forces
+{
+    public class ForceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
