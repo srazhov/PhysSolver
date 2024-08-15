48e3d1f3967703ee0975b176dc87ca41937b6781
srazhov <srazhov@icloud.com>
2024-08-15 23:52:03 +0500

    Added PhysObjectsParser class

---
+++ PhysHelper/PhysHelper/Parsers/PhysObjectsParser.cs
@@ -0,0 +1,29 @@
+﻿using PhysHelper.Parsers.PhysObjectParsers;
+using PhysHelper.Scenes.Objects;
+using PhysHelper.Scenes.SceneSettings;
+
+namespace PhysHelper.Parsers
+{
+    public class PhysObjectsParser
+    {
+        public static List<IPhysObject> Parse(SceneSettings settings)
+        {
+            // ObjectsCreationParser must always be first
+            IParserHandler<List<IPhysObject>, SceneSettings> parser = new ObjectsCreationParser();
+
+            parser
+                .SetNext(new WeightForceParser())
+                .SetNext(new NormalForceParser()) // NormalForceParser must be after WeightForce
+                .SetNext(new AdditionalForceParser())
+                .SetNext(new KineticFrictionForceParser()) // KineticFrictionForceParser must be after NormalForce
+                .SetNext(new ElasticForceParser());
+                
+
+            var result = new List<IPhysObject>();
+            parser.Parse(result, settings);
+
+            return result;
+        }
+    }
+}
+
